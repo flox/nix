f0e55a298f36243e7f0c49b2101cabdeb89d7294
Tom Bereknyei <tomberek@gmail.com>
2024-01-27 23:47:41 -0500

    stash script

---
+++ go.sh
@@ -0,0 +1,4 @@
+emconfigure ./configure --with-boost=$PWD/src/ports/boost_headers --disable-cpuid --disable-seccomp-sandboxing --enable-gc=no --disable-tests --prefix=$PWD/outputs/out --disable-doc-gen
+
+
+
